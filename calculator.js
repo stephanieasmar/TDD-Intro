bc3b9fd5499b68966189a69e11fb7793509a1ea4
Stephanie Asmar <sasmar8@gmail.com>
2017-08-07 15:12:37 -0400

    adding calculator file, changes to json package , changes to test.js

---
+++ calculator.js
@@ -0,0 +1,26 @@
+class Calculator {
+    constructor(){
+    }
+
+    add(num1, num2) {
+        return num1 + num2;
+    }
+
+    subtract(num1, num2) {
+        return num1 - num2;
+    }
+
+    multiply(num1, num2) {
+        return num1 * num2;
+    }
+
+    divide(num1, num2) {
+        return num1 / num2;
+    }
+
+    addAsync(num1, num2) {
+        return num1 + num2;
+    }
+}
+
+module.exports = Calculator; s
